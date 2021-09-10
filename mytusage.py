8915919a086f841011ebef1a44be449e73d4b5c2
sjdvda <iam@saajid.com>
2021-09-10 12:28:55 +0400

    v1.1
    Now compatible with Python 3.9, using latest MechanicalSoup & Apprise packages.

--- mytusage.py
+++ mytusage.py
@@ -10,13 +10,14 @@ from bs4 import BeautifulSoup
 
 __author__ = 'sjdvda'
 
-def get_myt_usage():
 
+def get_myt_usage():
     configfile = 'config.ini'
 
     config = configparser.ConfigParser()
 
-    if not( os.path.isfile(configfile)):
+    # Check if config file exists
+    if not (os.path.isfile(configfile)):
         print("Error: Config file not found.")
         sys.exit(1)
     else:
@@ -25,10 +26,9 @@ def get_myt_usage():
     # Get values from config file
     username = config['DEFAULT']['USERNAME']
     password = config['DEFAULT']['PASSWORD']
-    threshold = config['DEFAULT']['THRESHOLD']
-    time_format = config['FORMAT']['TIME']
     apprise_syntax = config['APPRISE']['SYNTAX']
-
+    time_format = config['FORMAT']['TIME']
+    threshold = config['DEFAULT']['THRESHOLD']
 
     # Create apprise object
     apprise_object = apprise.Apprise()
@@ -37,18 +37,17 @@ def get_myt_usage():
     # Set browser
     browser = mechanicalsoup.StatefulBrowser()
 
-    # Open my.t website
-    browser.open("http://internetaccount.myt.mu")
+    # Login to my.t website
+    browser.open("https://internetaccount.myt.mu")
 
     # Enter credentials
-    form = browser.select_form('.loginarea')
+    login_form = browser.select_form('#id3')
     browser["signInForm.username"] = username
     browser["signInForm.password"] = password
 
     # Sign in
-    form = browser.select_form()
-    form.choose_submit('signInContainer:submit')
-    resp = browser.submit_selected()
+    login_form.choose_submit('signInContainer:submit')
+    browser.submit_selected()
 
     # Load page
     page = browser.get_current_page()
@@ -63,21 +62,16 @@ def get_myt_usage():
         # Get values from page
         stats = (page.find_all("table")[5]).find_all("table")[1].find_all("td", bgcolor="#FFFFFF")
 
-        # Extract remaining data
-        remaining_data = stats[5]
-
-        # Regex black magic to extract the value
-        amt = float(re.findall(r'\d*\.?\d+', remaining_data.text)[0])
-
-        # Conversion to GB
-        gb_amount = amt * float(0.001)
-        remaining = str(round(gb_amount, 1)) + " GB"
-
-        # Extract expiry date
+        # Extract remaining volume allowance and expiry date
+        remaining_allowance = stats[5]
         expiry_date = stats[6]
 
+        # Extract the value and convert to GB
+        gb_amount = float(re.findall(r'\d*\.?\d+', remaining_allowance.text)[0]) * float(0.001)
+        gb_amount_readable = str(round(gb_amount, 1)) + " GB"
+
         # Date conversion
-        convert_date = dateparser.parse(expiry_date.text)
+        convert_date = dateparser.parse(expiry_date.text, settings={'TIMEZONE': 'GMT+4'})
 
         if time_format == "12h":
             converted_date = convert_date.strftime("%d/%m/%Y - %I:%M %p")
@@ -87,9 +81,8 @@ def get_myt_usage():
             print("Error: Invalid Time Format.")
             sys.exit(1)
 
-        message = 'Remaining Data: ' + remaining + '\nExpiry: ' + converted_date
+        message = 'Remaining Data: ' + gb_amount_readable + '\nExpiry: ' + converted_date
 
-        # Check if value exceeds threshold
         if threshold == '0' or float(threshold) <= gb_amount:
             # Send notification
             apprise_object.notify(
@@ -97,22 +90,20 @@ def get_myt_usage():
                 title='my.t Data Usage'
             )
         elif float(threshold) > gb_amount:
-            print("Value above threshold,  no notification sent")
-            print(message)
+            print("Value Above Threshold")
             sys.exit(0)
         else:
-            print("Error: Invalid Threshold Value.")
+            print("Invalid Threshold Value")
             sys.exit(1)
 
         print(message)
-
-        sys.exit(0)
 
     # If login fails, print error message from my.t portal
     else:
         error_message = page.select(".feedbackPanelERROR")[0]
         print("Error: Login Failed. ", error_message.text)
         sys.exit(1)
+
 
 if __name__ == '__main__':
     get_myt_usage()
